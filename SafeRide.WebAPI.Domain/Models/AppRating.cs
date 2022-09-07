767d84deef04819b89e6d0aa6c2428e356d93d29
kingugo007 <oluoanselm@gmail.com>
2022-09-07 18:10:36 +0100

    Added AppUser relationship to AppRating

--- SafeRide.WebAPI.Domain/Models/AppRating.cs
+++ SafeRide.WebAPI.Domain/Models/AppRating.cs
@@ -1,8 +1,12 @@
-﻿namespace SafeRide.WebAPI.Domain.Models
+﻿using SafeRide.WebAPI.Core.Models;
+
+namespace SafeRide.WebAPI.Domain.Models
 {
     public class AppRating
     {
         public int Id { get; set; }
         public int Rating { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }    
     }
 }
