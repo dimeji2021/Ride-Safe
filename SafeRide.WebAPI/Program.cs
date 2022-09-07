d1a266931942b9e764c5058ca44f84d0298edcde
dimeji2021 <adedimeji88@yahoo.com>
2022-09-07 21:21:35 +0100

    update EFCore

--- SafeRide.WebAPI/Program.cs
+++ SafeRide.WebAPI/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using SafeRide.WebAPI.Infrastructure.DataBase;
+
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
+builder.Services.AddDbContext<SafeRideContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
